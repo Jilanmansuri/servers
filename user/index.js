2445578983dc39488bde48a05b10455f6d3c9a81
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2026-02-23 15:28:52 +0530

    edit

--- user/index.js
+++ user/index.js
@@ -6,7 +6,6 @@ app.use(express.json());
 app.use(cors());
 
 
-
 const users = [
     { att: 80, uid: 108623, totalsub: 12, bonus: 20, name: "jilan" },
     { att: 100, uid: 108757, totalsub: 15, bonus: 30, name: "ammar" },
